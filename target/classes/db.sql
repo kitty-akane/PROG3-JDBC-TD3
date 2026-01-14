65a25839135930206197d6cc5bd1319de2ac61f5
sitraka <sitraka7011@gmail.com>
2026-01-14 04:11:36 +0300

    update: target classes

--- target/classes/db.sql
+++ target/classes/db.sql
@@ -2,5 +2,11 @@ CREATE DATABASE mini_dish_db;
 CREATE USER mini_dish_db_manager WITH PASSWORD 'mini_dish_pass';
 GRANT CONNECT ON DATABASE mini_dish_db TO mini_dish_db_manager;
 GRANT USAGE, CREATE ON SCHEMA public TO mini_dish_db_manager;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO mini_dish_db_manager;
 GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES
 ON ALL TABLES IN SCHEMA public TO mini_dish_db_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT SELECT, INSERT, UPDATE, DELETE, REFERENCES
+ON TABLES TO mini_dish_db_manager;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
+GRANT USAGE, SELECT ON SEQUENCES TO mini_dish_db_manager;
